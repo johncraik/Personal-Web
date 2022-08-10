10d177c624fddf7ae69f8f132afd5bc9499e0877
johncraik <jgcraik1@sheffield.ac.uk>
2022-08-10 21:58:43 +0100

    added javascript file and footer [6]

---
+++ scripts/java.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+  //Universal Footer:
+  var footerInfo = "Website created by John Craik - Visit my GitHub for development."
+  document.getElementById('footInfo').innerHTML = footerInfo;
+}
